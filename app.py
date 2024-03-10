5e8480754e5e47717bce6f5a83cb0e532ed7872f
lucky24rohilla <121234024+lucky24rohilla@users.noreply.github.com>
2024-03-10 15:15:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    image_url = "https://example.com/your_image.jpg"
+    image_url = "https://graph.org/file/13d8c0de8c97b7d295481.png"
     return f'<img src="{image_url}" alt="Your Image">'
 
 if __name__ == "__main__":
