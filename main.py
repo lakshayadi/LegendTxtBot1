43ab62753c34d887c5a08e22930e7ab5a2d580a0
lakshayadi <162904227+lakshayadi@users.noreply.github.com>
2024-03-10 21:37:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ from pyshorteners import Shortener
 
 botStartTime = time.time()
 bot = Client("bot",
-             bot_token= "6440702084:AAHX6_79ATiAm4LyrABvEyOOY4C6RKOpK-Y",   #KingLuminant_1
+             bot_token= "7156156781:AAFLcv7I2iGMjWbg-tjr0IkWXOV-SKTV8Es", #LegendTxtBot
              api_id= 20299588,
              api_hash= "f550d6179131c293d658f15f8c24f594")
 
