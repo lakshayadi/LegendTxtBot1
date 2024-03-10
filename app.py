bb639ac364a807fee759784d34c2ebfacf98b077
lucky24rohilla <121234024+lucky24rohilla@users.noreply.github.com>
2024-03-10 15:02:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'ʟʊʍɨռǟռȶ Txt Bot 2'
+    image_url = "https://example.com/your_image.jpg"
+    return f'<img src="{image_url}" alt="Your Image">'
 
-
-if __name__ == "ʟʊʍɨռǟռȶ Txt Bot 2":
+if __name__ == "__main__":
     app.run()
