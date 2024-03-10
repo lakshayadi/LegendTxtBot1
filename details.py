10cbbee089bee938f74343a3910f7a954e45ccec
lucky24rohilla <121234024+lucky24rohilla@users.noreply.github.com>
2024-03-10 15:16:56 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
 api_id = 20299588
 api_hash = "f550d6179131c293d658f15f8c24f594"
-bot_token = "7027553426:AAFahXChWsKijBFis8fJxjaTI6VFl6bIM2g"
+bot_token = "6440702084:AAHX6_79ATiAm4LyrABvEyOOY4C6RKOpK-Y"  #KingLiminant_1
 log_channel = -1001967910749
 
